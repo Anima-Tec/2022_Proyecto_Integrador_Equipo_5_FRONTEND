2208a07c6803ff54b31d2b124220d650424b23a5
camila-anima <108488699+camila-anima@users.noreply.github.com>
2022-09-02 08:25:51 -0300

    Uninstall sass and install chakra (#2)
    * uninstall sass
    
    * uninstall tailwind
    
    * install chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Box } from '@chakra-ui/react';
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <Box m={2}>
+        Tomato
+      </Box>
+      <Box maxW="960px" mx="auto" />
+      <Box m={[2, 3]} />
+      <div>App</div>
+    </>
   );
 }
